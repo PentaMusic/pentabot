374a32c952215728054f8d98bd033f1ba2fc28b4
May1Duke <abulopia@gmail.com>
2025-08-04 15:00:21 +0900

    weather test

--- server/agent/agent.js
+++ server/agent/agent.js
@@ -1,33 +1,35 @@
 import { createReactAgent } from "@langchain/langgraph/prebuilt";
 import { ChatAnthropic } from "@langchain/anthropic";
+import { tool } from "@langchain/core/tools";
+import { MemorySaver } from "@langchain/langgraph";
+import { z } from "zod";
 
-
-async function main() {
-  try {
-    const llm = new ChatAnthropic({
-      model: "claude-3-5-sonnet-latest",
-      // temperature: 0
-    });
-
-    const agent = createReactAgent({
-      llm,
-      tools: [],
-      prompt:
-        "You are a helpful assistant that can answer questions and help with tasks.",
-      verbose: true,
-    });
-
-    const result = await agent.invoke({
-      messages: [{ 
-        role: "user", 
-        content: "Hello, how can I help you?" }],
-    });
-
-    console.log(result);
-    console.log(result.messages.at(-1)?.content);
-  } catch (error) {
-    console.error("Error running agent:", error);
+const llm = new ChatAnthropic({
+  model: "claude-3-5-sonnet-latest",
+  temperature: 0.6
+});
+const weatherTool = tool(
+  async (city) => {
+    // const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPENWEATHER_API_KEY}`);
+    // const data = await response.json();
+    // return data;
+    return "It's 25 degrees and sunny.";
+  },
+  {
+    name: "weather",
+    description: "Get the weather in a given location",
+    parameters: z.object({
+      city: z.string().describe("The query to use in your search."),
+    }),
   }
-}
-// test push
-main();
+);
+const checkpointSaver = new MemorySaver();
+
+export const agent = createReactAgent({
+  llm,
+  tools: [weatherTool],
+  checkpointSaver,
+  verbose: true,
+});
+
+
