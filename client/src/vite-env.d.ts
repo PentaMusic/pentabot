0ac7448e56eeb72af4dc910cb46eb28df1f3f21b
May1Duke <abulopia@gmail.com>
2025-08-06 17:46:22 +0900

    환경수정
    yaml 수정!!!!

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,9 +1,1 @@
 /// <reference types="vite/client" />
-interface ImportMetaEnv {
-    readonly VITE_API_URL: string
-    // 다른 환경변수들도 여기에 추가할 수 있습니다
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv
-  }
